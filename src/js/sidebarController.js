f352b7a29d0054bc11248fc67fada9c569825e91
starkwang <381152119@qq.com>
2015-09-23 12:12:39 +0800

    fix sidebar

--- src/js/sidebarController.js
+++ src/js/sidebarController.js
@@ -1,15 +1,28 @@
 //侧边搜索栏
 angular.module('starkAPP')
-    .controller('sidebarController', ['$scope', '$rootScope','BaseService','$timeout','$location',
-        function($scope, $rootScope,BaseService, $timeout,$location) {
-            if($location.path() == '/main/'){
-                $scope.mainIsActive = true;
-            }
-            if($location.path() == '/all/'){
-                $scope.allIsActive = true;
+    .controller('sidebarController', ['$scope', '$rootScope', 'BaseService', '$timeout', '$location',
+        function($scope, $rootScope, BaseService, $timeout, $location) {
+            function refreshen() {
+                $scope.mainIsActive = $scope.allIsActive = $scope.collectionIsActive = false;
+                if ($location.path() == '/main/') {
+                    $scope.mainIsActive = true;
+                }
+                if ($location.path() == '/all/') {
+                    $scope.allIsActive = true;
+                }
+                if ($location.path() == '/collection/') {
+                    $scope.collectionIsActive = true;
+                }
             }
 
-            $scope.showSearch = function(){
+            refreshen();
+            
+            $scope.changeURL = function(url){
+                $location.url(url);
+                refreshen();
+            }
+
+            $scope.showSearch = function() {
                 $rootScope.$broadcast('showSearch');
             }
         }
