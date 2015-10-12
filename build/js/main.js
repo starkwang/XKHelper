6c7414da2c7ddb8a3316f40d52ad1a479d240bcc
starkwang <381152119@qq.com>
2015-10-12 15:47:20 +0800

    forum

--- build/js/main.js
+++ build/js/main.js
@@ -696,11 +696,16 @@ angular.module('starkAPP')
         }
     ]);
 
-//课程清单、考试时间
+//课程论坛
 angular.module('starkAPP')
     .controller('forumController', ['$scope', 'BaseService',
         function($scope, BaseService) {
-            
+
+            $scope.commentTitle = '点击此处可分享你的上课心得（已有999条评论）';
+            $scope.showComment = function(){
+                $scope.commentShow = true;
+                $scope.commentTitle = '发表评论';
+            }
         }
     ]);
 
